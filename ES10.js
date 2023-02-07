40fd45ad9f0a9ecc888a411c833edb7c39075a43
David Garcia <davidalexysg@gmail.com>
2023-02-06 20:24:49 -0700

    Added missing topic

--- ES10.js
+++ ES10.js
@@ -1,3 +1,17 @@
+//FLAT
+const array = [1,2,5,2,7,[5,8,3],[5,[1]]]
+console.log(array.flat(3))
+
+//Flat map first maps then flats
+const newArray = [1,5,7]
+console.log(newArray.flatMap((v)=>{
+return [v,v*5]
+}))
+
+//trim-start trim end
+const hello = '    Hello World!!!!    '
+console.log(hello.trimStart()+'a')
+console.log(hello.trimEnd()+'a')
 //catch
 try{
     hello()
